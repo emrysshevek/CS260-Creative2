ba3064cd509f43937ecffd78e090888a05796413
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-10-03 21:48:36 +0000

    very basic outline

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     
     $("#start").click(function(e){
-        $("#intro").hide();
-        $.getJSON("http://jservice.io/api/random", function(data) {
-            console.log(data);
-        })
+        
+        document.location.href = "../html/trivia.html";
+        // $("#intro").hide();
+        // $.getJSON("http://jservice.io/api/random", function(data) {
+        //     console.log(data);
+        // })
         e.preventDefault();
     })
     
