5ed687cc00186a95a55f0d820edb5b047d49c2e8
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-10-04 02:32:49 +0000

    added some style and progress bar

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
     
     $("#start").click(function(e){
-        
         document.location.href = "../html/trivia.html";
-        // $("#intro").hide();
-        // $.getJSON("http://jservice.io/api/random", function(data) {
-        //     console.log(data);
-        // })
         e.preventDefault();
     })
     
